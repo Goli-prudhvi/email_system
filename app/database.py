fb63e5f7cf6d1886eb8d5b7271e9f3dc4740f0ce
Goli Prudhvi <prudhvi.goli@hexanovamedia.tech>
2026-01-12 18:11:54 +0530

    updated database

--- app/database.py
+++ app/database.py
@@ -57,38 +57,67 @@
 
 
 
+# # app/database.py
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# from urllib.parse import quote_plus
+# from config import get_postgres_config
+
+# Base = declarative_base()
+
+
+# def create_db_engine():
+#     cfg = get_postgres_config()
+
+#     if not all(cfg.values()):
+#         raise RuntimeError("PostgreSQL environment variables are missing")
+
+#     password = quote_plus(cfg["password"])
+
+#     DATABASE_URL = (
+#         f"postgresql+psycopg2://{cfg['user']}:{password}"
+#         f"@{cfg['host']}:{cfg['port']}/{cfg['database']}"
+#         "?sslmode=require"
+#     )
+
+#     return create_engine(
+#         DATABASE_URL,
+#         pool_pre_ping=True,
+#         pool_size=5,
+#         max_overflow=10,
+#     )
+
+
+# engine = create_db_engine()
+
+# SessionLocal = sessionmaker(
+#     bind=engine,
+#     autocommit=False,
+#     autoflush=False,
+# )
+
+
+
+
+
 # app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from urllib.parse import quote_plus
-from config import get_postgres_config
 
 Base = declarative_base()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-def create_db_engine():
-    cfg = get_postgres_config()
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set in environment variables")
 
-    if not all(cfg.values()):
-        raise RuntimeError("PostgreSQL environment variables are missing")
-
-    password = quote_plus(cfg["password"])
-
-    DATABASE_URL = (
-        f"postgresql+psycopg2://{cfg['user']}:{password}"
-        f"@{cfg['host']}:{cfg['port']}/{cfg['database']}"
-        "?sslmode=require"
-    )
-
-    return create_engine(
-        DATABASE_URL,
-        pool_pre_ping=True,
-        pool_size=5,
-        max_overflow=10,
-    )
-
-
-engine = create_db_engine()
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10,
+)
 
 SessionLocal = sessionmaker(
     bind=engine,
